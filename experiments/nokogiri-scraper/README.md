1e6301f9767adf0b03ce976597f311debfd70a25
Colin Loretz <colin@hackforreno.org>
2022-08-06 11:19:38 -0700

    initial nokogiri scraper

---
+++ experiments/nokogiri-scraper/README.md
@@ -0,0 +1,22 @@
+# City Government Agenda Scraper
+
+This scraper is designed to pull down meetings from iqm2 hosted agenda and meeting minutes.
+
+## Getting Started
+Using rvm or rbenv make sure you have the ruby version defined in .ruby-version.
+
+### Install the gems
+```
+bundle install
+```
+
+### Setup a .env
+
+Create a `.env` file based on the `sample.env`.
+
+The `CITY_PORTAL_URL` should be the iqm2 portal calendar URL.
+
+```
+# Example for the City of Reno IQM2 portal with dates from 1/1/1900 to 12/31/9999
+CITY_PORTAL_URL="http://renocitynv.iqm2.com/Citizens/calendar.aspx?From=1/1/1900&To=12/31/9999"
+```
---
+++ experiments/nokogiri-scraper/scraper.rb
@@ -0,0 +1,74 @@
+require 'uri'
+require 'dotenv/load'
+require 'net/http'
+require 'byebug'
+require 'nokogiri'
+
+def parse_meeting(meeting)
+  
+  datetime = meeting.css('div.RowLink a').text
+  meeting_body = meeting.css('div.RowBottom .RowDetails').text
+
+  is_cancelled = meeting.css('span.MeetingCancelled').empty? ? false : true
+  
+  agenda, agenda_packet, minutes, journal, video = ""
+
+  if !is_cancelled
+    meeting_links = meeting.css('div.MeetingLinks a')
+
+    agenda = meeting_links[0].attributes["href"].value
+    agenda_packet = meeting_links[1].attributes["href"].value
+    minutes = meeting_links[2].attributes["href"].value
+    journal = meeting_links[3].attributes["href"].value
+    video = meeting_links[4].attributes["href"].value
+  end
+
+  # date
+  # time
+  # location
+  # meeting_body
+  # is_cancelled
+  # agenda
+  # agenda_packet
+  # minutes
+  # journal
+  # agenda_table
+  # agenda_item_timestamp
+  # agenda_items
+  # meeting_attachments
+
+  { datetime: datetime, meeting_body: meeting_body, agenda: agenda, agenda_packet: agenda_packet, minutes: minutes, journal: journal, video: video }
+end
+
+def download_meeting(meeting)
+  # download the meeting
+  # download the agenda
+  # download the agenda packet
+  # download the minutes
+  # download the journal
+  # download the video
+  puts meeting
+
+  # follow each link and download the file
+  # how can we get the video?
+
+  # save these to S3 by meeting
+end
+
+uri = URI.parse(ENV["CITY_PORTAL_URL"])
+
+response = Net::HTTP.get_response(uri)
+html = response.body
+
+doc = Nokogiri::HTML(html)
+
+meeting_rows = doc.css('.Row.MeetingRow')
+
+test_meeting = meeting_rows.first
+
+meeting_rows.each do |meeting_row|
+  meeting = parse_meeting(meeting_row)
+  download_meeting(meeting)
+end
+
+puts "Finished"
